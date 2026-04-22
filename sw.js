22db7324b2a3b4334d3fb3ae8d4a1ba8f4c15030
jgemayel <jimmyelgemayel@gmail.com>
2026-04-22 09:39:18 +0000

    Deploy: deduped cycles + anchored X-axis dates

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 
-const CACHE = 'jimmy-health-v6';
+const CACHE = 'jimmy-health-v7';
 self.addEventListener('install', (e) => { self.skipWaiting(); });
 self.addEventListener('activate', (e) => {
   e.waitUntil((async () => {
